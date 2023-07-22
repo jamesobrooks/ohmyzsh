d93efbf6366e2b4347713638984660173bd863bf
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-07-21 21:51:50 -0500

    Add aliases and functions

---
+++ custom/example-path.zsh
@@ -0,0 +1,11 @@
+#GRAALVM_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java17-22.3.1/Contents/Home
+#JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java17-22.3.1/Contents/Home
+#PATH=$JAVA_HOME/bin:$PATH
+
+#PATH=$(pyenv root)/shims:$PATH
+#PATH=/Users/jamesbrooks/Tools:$PATH
+
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
