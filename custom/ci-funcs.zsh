ad9a9666ebf6f51d60df8b05ac9a677f0696fad9
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-03-01 09:02:01 -0600

    Remove unused function

--- custom/ci-funcs.zsh
+++ custom/ci-funcs.zsh
@@ -26,36 +26,6 @@ function runcamsci() {
   git commit -m "CAMS-$story: $message" "$@"
 }
 
-function team-commit() {
-  local append team_member
-    if (( $# == 0 )); then
-      exit 1
-    fi
-  append="\n\n"
-  while true; do
-    if (( $# == 0 )); then
-      break
-    fi
-    team_member=$(team $1); shift
-    append="${append}Co-authored-by: ${team_member}\n"
-  done
-  echo -e "$append"
-}
-
-function team() {
-  case $1 in
-    'arthur') echo "Arthur Morrow <amorrow@Arthurs-MacBook-Pro.local>";;
-    'james') echo "James Brooks <12275865+jamesobrooks@users.noreply.github.com>";;
-    'fritz') echo "Fritz Madden <fmadden@flexion.us>";;
-    'mike') echo "Michael Ly <105381067+magile32@users.noreply.github.com>";;
-    'stan') echo "Stanley Smith <stancsmith@gmail.com>";;
-    'swathi') echo "SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>";;
-    *) echo "${1} was not recognized as a member of your team"; return 1;;
-  esac
-
-  return 0
-}
-
 prcams() {
   if [[ -n $1 ]]; then
     branch=$1
